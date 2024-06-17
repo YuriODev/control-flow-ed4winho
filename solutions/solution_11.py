6249540a05577e17389d7b5db2a22380b25f9c90
ed4winho <gojijoseph1@gmail.com>
2024-06-17 11:52:06 +0100

    Update solution_11.py

--- solutions/solution_11.py
+++ solutions/solution_11.py
@@ -1,9 +1,5 @@
-# Prompt user to enter a year and determine if it is a leap year or not.
-year = int(input("Enter a year: "))
-
-
-# Check if the year is divisible by 4 and not divisible by 100, or if it is divisible by 400
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("Leap year.")
+year=int(input("enter the year:"))
+if year % 4==0:
+   print("leap year")
 else:
-    print("Ordinary year.")
+   print('ordinary year')
