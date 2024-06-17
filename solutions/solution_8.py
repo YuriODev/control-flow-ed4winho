c429094987d09ba0b71d326a8c8ed0771f8311ef
ed4winho <gojijoseph1@gmail.com>
2024-06-17 11:50:31 +0100

    Update solution_8.py

--- solutions/solution_8.py
+++ solutions/solution_8.py
@@ -1,14 +1,15 @@
-# Prompt the user to enter a three-digit number and store it in the variable 'number'
+def check_digit_in_number(num, digit):
+  num_str = str(num)
+  digit_str = str(digit)
+
+  for char in num_str:
+      if char == digit_str:
+          return True
+
+  return False
+
 number = int(input("Enter a three-digit number: "))
+digit = int(input("Enter a digit to check: "))
 
-# Prompt the user to enter a digit and store it in the variable 'digit'
-digit = int(input("Enter a digit: "))
-
-# Check if the entered digit is present in the three-digit number
-# The expression digit == number % 10 checks if the digit is equal to the last digit of the number
-# The expression digit == (number // 10) % 10 checks if the digit is equal to the middle digit of the number
-# The expression digit == number // 100 checks if the digit is equal to the first digit of the number
-is_in_number = digit == number % 10 or digit == (number // 10) % 10 or digit == number // 100
-
-# Print whether the digit is present in the number
-print(is_in_number)
+result = check_digit_in_number(number, digit)
+print(result)
