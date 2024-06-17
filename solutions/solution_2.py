5e2dae6885884bdcaeba5459afbc4348f4601719
ed4winho <gojijoseph1@gmail.com>
2024-06-17 11:47:00 +0100

    Update solution_2.py

--- solutions/solution_2.py
+++ solutions/solution_2.py
@@ -1,18 +1,11 @@
 # Prompt the user to enter their age and convert it to an integer
-age = int(input("Enter your age: "))
-
-
-# Check the age of the user and print an appropriate message
+age=int(input("input your age:"))
 if age <= 1:
-    # If the age is less than or equal to 1, the user is an infant
-    print("You are an infant.")
-# If the age is greater than 1 and less than 13, the user is a child
-elif age < 13:
-    # If the age is less than 13, the user is a child
-    print("You are a child.")
-elif age < 20:
-    # If the age is less than 20, the user is a teenager
-    print("You are a teenager.")
-else:
-    # If the age is 20 or greater, the user is an adult
-    print("You are an adult.")
+  print("you are a inphant:")
+elif age > 1 and age<13:
+  print("you are a child")
+elif age >= 13 and age<20:
+  print('you are a teenager')
+elif age>20:
+  print("you are an adult")
+
